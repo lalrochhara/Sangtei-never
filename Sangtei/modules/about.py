81381814adc3df611335e30b413001a065c28097
Nicky Lalrochhara <nickylalrochhara917@gmail.com>
2023-08-05 20:03:11 +0530

    Add files via upload

---
+++ Sangtei/modules/about.py
@@ -0,0 +1,18 @@
+__help__ = """
+
+* Sangtei 👸 - Anime Themed Powerful Telegram Group Manager Bot.*
+
+✮───────────────✮
+𝐏𝐎𝐖𝐄𝐑𝐅𝐔𝐋 𝐀𝐁𝐈𝐋𝐈𝐓𝐈𝐄𝐒
+✮───────────────✮
+• File To Link and URL Upload 📂
+• Youtube Downloader 🎵
+• Serach Books 📚
+• Search Exam Results 🏆
+
+♟ Developed by :- [Sangtei Team](https://t.me/SangteiUpdates)
+♟ Source Code :-[Click Here](https://t.me/ZoYakumoUpdates/1013)
+
+ ©️@SangteiBot
+"""
+__mod_name__ = "ABOUT🔆"
